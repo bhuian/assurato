171d9658d2011997d9abe00614f65f9115890b26
Zubair <74538410+zubairbhuian@users.noreply.github.com>
2022-12-06 17:22:49 +0600

    12/6/22.1

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,3 +1,30 @@
-$(".bars").click(function(){
+
+$(document).ready(function() {
+  $(".bars").click(function(){
     $(".appbar").toggleClass("show");
+  });
+  $(".login-med-header").click(function(){
+    $(".login-med-header-btns").toggleClass("show");
+  });
+  $(".login-med-header-btns ul li").click(function () {
+      var medText=$(this).text();  
+      $('.login-med-select').text(medText);
+      $(".login-med-header-btns ul li").removeClass("active");
+      $(this).addClass("active");  
+  });
+   $('.login-med-mit-id').click(function(){
+    $('#med-mit-id').addClass('show');
+    $('#med-e-mail').removeClass('show');
+    $('#med-ind').removeClass('show');
+   })
+   $('.login-med-e-mail').click(function(){
+    $('#med-mit-id').removeClass('show');
+    $('#med-e-mail').addClass('show');
+    $('#med-ind').removeClass('show');
+   })
+   $('.login-med-ind').click(function(){
+    $('#med-mit-id').removeClass('show');
+    $('#med-e-mail').removeClass('show');
+    $('#med-ind').addClass('show');
+   })
   });
