6277de1312e7868e8b831d20249188ebaa42942d
Zubair <74538410+zubairbhuian@users.noreply.github.com>
2022-12-08 19:23:04 +0600

    12/8/22.4

--- assets/js/app.js
+++ assets/js/app.js
@@ -28,3 +28,22 @@ $(document).ready(function() {
     $('#med-ind').addClass('show');
    })
   });
+
+
+
+  // 
+  $(document).ready(function(){
+    $('.accordion-list > li > .answer').hide();
+      
+    $('.accordion-list > li').click(function() {
+      if ($(this).hasClass("active")) {
+        $(this).removeClass("active").find(".answer").slideUp();
+      } else {
+        $(".accordion-list > li.active .answer").slideUp();
+        $(".accordion-list > li.active").removeClass("active");
+        $(this).addClass("active").find(".answer").slideDown();
+      }
+      return false;
+    });
+    
+  });
